c1a1fffe6d1c3b9499acd05d1042fc70695e655d
yewon <yewone0428@naver.com>
2024-05-17 17:13:21 +0900

    signIn imageField add - yw

--- finance_pjt_back/finance_pjt/settings.py
+++ finance_pjt_back/finance_pjt/settings.py
@@ -160,7 +160,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
